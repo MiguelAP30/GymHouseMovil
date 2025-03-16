6019c68e48bb8d2eba4b33545e92ed2fd3498158
MiguelAP30 <miguelonperez300@gmail.com>
2025-03-16 00:16:11 -0500

    git ignore

--- config/env.ts
+++ config/env.ts
@@ -1,6 +1,6 @@
 export const ENV = {
   development: {
-    API_URL: 'http://192.168.80.14:8000/api/v1',
+    API_URL: 'http://0.0.0.0:8000/api/v1',
   },
   production: {
     API_URL: 'http://192.168.80.14:8000/api/v1', // Cambiar a la URL de producción cuando esté disponible
