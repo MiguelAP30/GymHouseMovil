2bac8a9c3c05528fe1f3a12659db10dcd384f9b7
andresperez2003 <andresap2017@gmail.com>
2025-04-01 21:17:24 -0500

    Feature:Despliegue

---
+++ config/env.ts
@@ -0,0 +1,18 @@
+export const ENV = {
+  development: {
+    API_URL: 'https://apigymhouse.lat/api/v1',
+    //192.168.1.2
+    //192.168.80.14
+  },
+  production: {
+    API_URL: 'https://apigymhouse.lat/api/v1', // Cambiar a la URL de producción cuando esté disponible
+  },
+  test: {
+    API_URL: 'https://apigymhouse.lat/api/v1',
+  },
+};
+
+// Por defecto usamos el entorno de desarrollo
+export const getEnvironment = () => {
+  return ENV.development;
+};
